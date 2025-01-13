dd69822507628f8f0b21d23f50d41733b5c1dc01
tvpanchal0 <128672578+tvpanchal0@users.noreply.github.com>
2025-01-13 22:13:29 +0530

    Update api.js
    api Url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_URL = 'todo-backend-nq8cglrmk-tvpanchal0s-projects.vercel.app';
+const API_URL = 'https://todo-backend-app-liard.vercel.app';
 
 // Axios instance with default settings
 const api = axios.create({
